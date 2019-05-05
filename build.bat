123e8cbac1a29b339ac5ca8ab641501ebad605ba
verner214 <lars.waernulf@gmail.com>
2019-05-05 10:34:11 +0200

    extra laesning2

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
 copy header1.html + .\blgscheduledpages\blghead.html + header2.html + .\blgscheduledpages\blgbody.html + footer.html .\blgscheduledpages\index.html
+copy header1.html + .\blgindexingfiles\blghead.html + header2.html + .\blgindexingfiles\blgbody.html + footer.html .\blgindexingfiles\index.html
